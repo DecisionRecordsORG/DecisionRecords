b5c1c6751f36ba768e15acd46d4520fe8e108d1e
Lawrence Nyakiso <lawrencepn@users.noreply.github.com>
2026-01-08 22:50:46 +0100

    Bump version to 1.14.1

--- version.py
+++ version.py
@@ -14,7 +14,7 @@ import json
 from datetime import datetime
 
 # Application version - automatically updated by git pre-commit hook
-__version__ = "1.14.0"
+__version__ = "1.14.1"
 
 # Build metadata
 __build_date__ = "2026-01-08"
